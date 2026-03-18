836d8ff4bfb4a6ad51fbbcc5393905f2e2fb08b0
umesh2341 <bgmiop139@gmail.com>
2026-03-18 23:51:30 +0530

    able to greet

--- process/steps/google.js
+++ process/steps/google.js
@@ -1,13 +1,37 @@
 import services from '../config.js';
+import SerperAI from './search.js';
 
 const {geminiAI}=services;
+const Aifunction={
+    async genReply(message){
+         try{
+            const data= await SerperAI(message);
+ let add=[];
+ const datas=data.organic.map(item=>{
+    if(item.position<=4){
+          add.push(item.snippet)
+    }else{
+        return;
+    }
+});
+const summery=add.join()
+         const response = await geminiAI.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: `
+User name: ${message}
 
-async function main() {
-    const response = await geminiAI.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "hi, this is test message that you are woking",
-  });
-  console.log(response.text);
+Relevant search data: ${summery}
+
+you are a help full assitant, you must first greet user and ask them if you can help using the relevent data
+        `,
+      });
+      return response.text;
+
+         }catch(err){
+            console.log("Gemini error",err)
+            return "somthing went wrong"
+         }
+    },
 }
-
-main();
+const test=await Aifunction.genReply("ELon musk")
+console.log(test);
