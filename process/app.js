e0017c1a1ce5612654be6ea70b427f3ee94a8653
umesh2341 <bgmiop139@gmail.com>
2026-03-19 14:04:40 +0530

    started server

--- process/app.js
+++ process/app.js
@@ -1,4 +1,17 @@
 import express from 'express';
-import services from '../config'
-
+import services from './config.js'
+import cors from "cors"
+import  {dbService} from './steps/db.js'
+import dotenv from "dotenv"
 const {geminiAI,DataBase}=services;
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.get('/',async(req,res)=>{
+    res.send("server is running")
+})
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log("Server is listining on port", PORT)
+})
